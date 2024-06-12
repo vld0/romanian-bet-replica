87f1bd0c831b1fca560a7663c76cf56acf92e1c4
Catalin Vlad <catalin.vlad@ext.ec.europa.eu>
2024-06-12 21:38:53 +0200

    fix bug amount to spend

--- replicate.js
+++ replicate.js
@@ -12,8 +12,8 @@ const {w3cwebsocket: WebSocket} = require("websocket");
  */
 
 //how many companies you want to replicate
-var myBetSize = process.env.topCompanies;
-var amountToInvest = process.env.amountToInvest;
+var reducedBetSize = process.env.topCompanies;
+var amountToInvest = parseInt(process.env.amountToInvest);
 
 var user = process.env.user;
 var password = process.env.password;
@@ -23,9 +23,11 @@ replicate();
 
 function replicate() {
 
-  if (!user || !password || !myBetSize || !amountToInvest) {
+  if (!user || !password || !reducedBetSize || !amountToInvest) {
     throw new Error("You must provide the following input parameters: user password betSize amountToInvest");
   }
+
+  console.log("You want to invest " + amountToInvest + " lei.\n");
 
   Promise.all([tradeville.loadMyPortfolio(user, password, demo), bvbParser.loadIndexComposition()])
     .then(responses => {
@@ -33,11 +35,11 @@ function replicate() {
       let portfolio = responses[0];
       let bet = responses[1];
 
-      if (bet.length < myBetSize) {
-        throw new Error("you are trying to replicate " + myBetSize + " companies while BET index has only " + bet.length);
+      if (bet.length < reducedBetSize) {
+        throw new Error("you are trying to replicate " + reducedBetSize + " companies while BET index has only " + bet.length);
       }
 
-      //console.log("[START] Your Tradeville portfolio\n" + prettyPrint(portfolio) + "[END] Your Tradeville portfolio.\n\n");
+      console.log("[START] Your Tradeville portfolio\n" + prettyPrint(portfolio) + "[END] Your Tradeville portfolio.\n");
       //console.log("[START] BET index\n" + prettyPrint(bet) + "[END] BET index\n\n");
 
       //all symbols from bet
@@ -54,35 +56,43 @@ function replicate() {
       for (i = 0; i < betPortfolio.length; i++) {
         tmpBetSize = betSymbols.indexOf(betPortfolio[i].symbol);
         if (tmpBetSize > calculatedBetSize) {
+          //console.log("a="+tmpBetSize);
           calculatedBetSize = tmpBetSize;
         }
       }
-      if (calculatedBetSize > myBetSize) {
-        console.log("WARNING!!! You are going to replicate less companies " + myBetSize + " that you currently have in your portfolio " + calculatedBetSize + ".\n");
+      if ((calculatedBetSize+1) > reducedBetSize) {
+        console.log("WARNING!!! You are going to replicate companies " + reducedBetSize + " but the lowest position in your portfolio is " + (calculatedBetSize+1) + ".\n");
       } else {
-        console.log("You are going to replicate " + myBetSize + " companies and it seems you currently have in your portfolio " + calculatedBetSize + ".\n");
+        console.log("You are going to replicate " + reducedBetSize + " companies and the lowest position in your portfolio is " + (calculatedBetSize+1) + ".\n");
       }
 
-      //extract top 'myBetSize' companies from BET;
+      //extract top 'reducedBetSize' companies from BET;
       //these are the top companies you are replicating
-      var myBet = bet.slice(0, myBetSize);
-      //sum up the weight of the top 'myBetSize' companies
-      var myBetWeight = myBet.map(p => p.weight).reduce((a, b) => a + b);
+      var reducedBet = bet.slice(0, reducedBetSize);
+      //sum up the weight of the top 'reducedBetSize' companies
+      var reducedBetWeight = reducedBet.map(p => p.weight).reduce((a, b) => a + b);
 
-      console.log("The total weight of top " + myBetSize + " companies is " + myBetWeight + ".\n");
+      let betPortfolioSymbols = betPortfolio.map(item => item.symbol);
+      let reducedBetSymbols = reducedBet.map(item => item.symbol);
+      if (betPortfolioSymbols.length < reducedBetSymbols.length) {
+        let difference = reducedBetSymbols.filter(x => !betPortfolioSymbols.includes(x));
+        console.log("The following symbols are missing from your portfolio: "+difference+" \n");
+      }
+
+      console.log("The total weight of top " + reducedBetSize + " companies is " + reducedBetWeight + ".\n");
 
       //recalculate the weight of every stock for the reduced BET
       //and enrich myBetPortfolio with the new value
-      myBet.forEach(item => item.myWeight = ((item.weight / (myBetWeight)) * 100));
+      reducedBet.forEach(item => item.reducedWeight = ((item.weight / (reducedBetWeight)) * 100));
 
       //this is for testing
-      const oneHundred = myBet.map(p => p.myWeight).reduce((a, b) => a + b);
+      const oneHundred = reducedBet.map(p => p.reducedWeight).reduce((a, b) => a + b);
       if (Math.abs(oneHundred - 100) > 0.01) {
         throw new Error("wrong calculation of weight " + oneHundred);
       }
 
-      console.log("[START] Your " + myBetSize + " Tradeville BET \n" + prettyPrint(myBet)
-        + "[END] Your " + myBetSize + " Tradeville BET portfolio.\n\n");
+      console.log("[START] Reduced " + reducedBetSize + " BET \n" + prettyPrint(reducedBet)
+        + "[END] Reduced " + reducedBetSize + " BET.\n\n");
 
       //calculate the value of your portfolio
       //add amountToInvest
@@ -98,15 +108,15 @@ function replicate() {
           throw new Error("could not find symbol " + item.symbol);
         }
       }).reduce((a, b) => a + b);
-      //console.log("Your current Tradeville BET portfolio value is " + currentPortfolioValue);
+      console.log("Your current Tradeville BET portfolio value is " + currentPortfolioValue);
 
       //add amountToInvest
       const totalAmount = currentPortfolioValue + amountToInvest;
 
       //then calculate how many stocks you can buy
       //make a difference with your existing portfolio
-      const toBuy = myBet.map(item => {
-        const finalQuantity = totalAmount * item.myWeight / 100 / item.price;
+      const toBuy = reducedBet.map(item => {
+        const finalQuantity = totalAmount * item.reducedWeight / 100 / item.price;
         //do I have the symbol in my portfolio ?
         const symbolPortfolio = betPortfolio.find(itemp => itemp.symbol === item.symbol);
         const currentQuantity = symbolPortfolio ? symbolPortfolio.quantity : 0;
@@ -115,7 +125,9 @@ function replicate() {
           'symbol': item.symbol,
           'finalQuantity': finalQuantity,
           'currentQuantity': currentQuantity,
+          'currentValue': currentQuantity * item.price,
           'NB_OF_STOCKS_TO_BUY': delta,
+          'AMOUNT_TO_SPEND': delta * item.price ,
           'price': item.price
         }
       });
